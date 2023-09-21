bea78a2e16e54d2714b9bdcac28246d8a8324ba6
Agosina Merino <agosm.am@gmail.com>
2023-09-21 14:20:47 -0300

    fetch, API en funcionamiento y correcciones finales

--- js/index.js
+++ js/index.js
@@ -45,92 +45,88 @@ props.push(new Propiedad(
 
 //validar formulario para evitar recarga del sitio al enviar
 
-const $formulario = document.querySelector('#form')
-$formulario.addEventListener('submit', validarFormulario) 
-async function validarFormulario(evt) {
-    evt.preventDefault()
-    const form = new FormData(this)
-    const respuesta = await fetch("https://formspree.io/f/xleyneor",{
-        method: this.method,
-        body: form,
-        headers:{
-            'Accept': 'application/json'
-        }
-        })
-        if (respuesta.ok) {
-            let obtenerDatos = () => {
-            let error = validar()
-            if (error == true) {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'Por favor complete los campos requeridos',
-                    imageUrl: "https://www.quever.news/u/fotografias/m/2020/12/24/f768x1-4793_4920_0.jpg",
-                    imageWidth: 300,
-                    imageHeight: 200,
-                    imageAlt: "campoIncompleto"
-                })
-            } else {
-                Swal.fire({
-                    title: '¡Excelente!',
-                    text: 'En breve nos comunicaremos para ofrecerte mas información',
-                    imageUrl: "https://media.lacapital.com.ar/p/f2d9862954fe0e332a2052192197c945/adjuntos/203/imagenes/030/624/0030624559/642x0/smart/friends-chandlerjpg.jpg",
-                    imageWidth: 300,
-                    imageHeight: 200,
-                    imageAlt: "campoCompleto"
-                })
-            }
-        }
-
-        let validar = () => {
-            let nombre = document.getElementById('nomYApe')
-            let telef = document.getElementById('telefono')
-            let error = false
-
-            if (nombre?.value == ' ' || nombre?.value == 'undefined' || nombre?.value == 'hola' || nombre?.value == 'null'|| telef?.value == '' || telef?.value <= 999999) {
-                error = true
-            }
-            return error
-        }
-
-        $formulario.addEventListener('submit', obtenerDatos);
-
-    }
-    
+let botonEnviar = document.getElementById('form')
+botonEnviar.addEventListener('submit', validarFormulario) 
+function validarFormulario(evt) {
+    evt.preventDefault()                     
 }
+
+
+let obtenerDatos = () => {
+    let error = validar()
+    if (error == true) {
+        Swal.fire({
+            title: 'Error',
+            text: 'Por favor complete los campos requeridos',
+            imageUrl: "https://www.quever.news/u/fotografias/m/2020/12/24/f768x1-4793_4920_0.jpg",
+            imageWidth: 300,
+            imageHeight: 200,
+            imageAlt: "campoIncompleto"
+        })
+    } else {
+        Swal.fire({
+            title: '¡Excelente!',
+            text: 'En breve nos comunicaremos para ofrecerte mas información',
+            imageUrl: "https://media.lacapital.com.ar/p/f2d9862954fe0e332a2052192197c945/adjuntos/203/imagenes/030/624/0030624559/642x0/smart/friends-chandlerjpg.jpg",
+            imageWidth: 300,
+            imageHeight: 200,
+            imageAlt: "campoCompleto"
+        })
+    }
+}
+
+let validar = () => {
+    let nombre = document.getElementById('nomYApe')
+    let telef = document.getElementById('telefono')
+    let error = false
+
+    if (nombre?.value == 'null' || nombre?.value == 'undefined' || nombre?.value == '' || telef?.value == '' || telef?.value <= 999999) {
+        error = true
+    } else{
+    }
+    return error
+}
+
+botonEnviar.addEventListener('submit', obtenerDatos);
 
 let inputNom = document.getElementById('nomYApe') 
 inputNom.addEventListener('change',function(){
-    console.log(inputNom.value)
-    let nomApLS = localStorage.setItem('nomApe',JSON.stringify(inputNom.value))
-    console.log("contenido del locastorage:" + JSON.parse(localStorage.getItem('nomApe')))
+    let nomApLS = localStorage.setItem('nomApe',inputNom.value)
 })
 let inputTel = document.getElementById('telefono')
 inputTel.addEventListener('change',function(){
-    console.log(inputTel.value)
     let telefLS = localStorage.setItem('tel',JSON.stringify(inputTel.value))
-    console.log("el telefono guardado en local storage es: " + JSON.parse(localStorage.getItem('tel')))
 })
-  
+
 let nomApe
 let nomApeLS = localStorage.getItem('nomApe')
-nomApeLS ? nomApe = nomApeLS : console.log('nombre y apellido: ' + nomApe || 'Aun no se ha registrado ningun nombre')
+// nomApeLS ? nomApe = nomApeLS : console.log('nombre y apellido: ' + nomApe || 'Aun no se ha registrado ningun nombre')
 
 let tel
 let telLS = localStorage.getItem('tel')
-telLS ? tel = telLS : console.log("telefono:" + tel || 'Aun no se ha registrado ningun telefono')
+// telLS ? tel = telLS : console.log("telefono:" + tel || 'Aun no se ha registrado ningun telefono')
+
+if(nomApeLS && telLS){
+    nomApe = nomApeLS
+    tel = JSON.parse(telLS)
+    let bienvenidaUsuario = document.getElementById('datosCliente')
+    let saludoUsuario = document.createElement('div')
+    saludoUsuario.innerHTML=`
+        <p> \n ¡Hola de nuevo ${nomApe}, recordá que ya enviaste una consulta adjuntando el número de telefono ${tel}!</p>
+    `
+    bienvenidaUsuario.appendChild(saludoUsuario)
+}
+else {
+}
 
 
 let fechaIng = document.getElementById('fechaIn')
 fechaIng.onchange= ()=>{
     let ingreso=fechaIng.value
-    console.log(ingreso)
 }
 
 let fechaSali = document.getElementById('fechaSal')
 fechaSali.onchange= ()=>{
     let salida = fechaSali.value
-    console.log(salida) 
 }
-
-                 
 
